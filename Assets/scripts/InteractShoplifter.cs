694f52ef49b44d767173b06ac056a2bc543756f9
hxttm <S10273775@connect.np.edu.sg>
2026-08-12 02:46:01 +0800

    couldnt fix cctv script

--- Assets/scripts/InteractShoplifter.cs
+++ Assets/scripts/InteractShoplifter.cs
@@ -1,47 +1,99 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
 
-public class CashierGameManager : MonoBehaviour
+public class NPCSellerTalk : MonoBehaviour
 {
-    // Singleton instance so other scripts can access it easily
-    public static CashierGameManager Instance { get; private set; }
+    [Header("UI & Dialogue")]
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialoguePanel;
+    public GameObject pressEPrompt;
 
-    [Header("Game Settings")]
-    public int currentScore = 10;
+    [Header("Settings")]
+    public Transform playerTransform;
+    public Transform walkAwayTarget; // Where she walks after saying bye
 
-    private void Awake()
+    private NavMeshAgent agent;
+    private Animator animator;
+    private int dialogueStep = 0;
+    private bool isPlayerInRange = false;
+
+    void Awake()
     {
-        // Setup Singleton pattern
-        if (Instance == null)
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        if (dialoguePanel) dialoguePanel.SetActive(false);
+        if (pressEPrompt) pressEPrompt.SetActive(false);
+    }
+
+    void Update()
+    {
+        // When player is near and presses E
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            Instance = this;
+            AdvanceDialogue();
         }
-        else
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            isPlayerInRange = true;
+            if (pressEPrompt) pressEPrompt.SetActive(true);
+            
+            // Stop NPC movement and face player
+            if (agent) agent.isStopped = true;
+            if (animator) animator.SetFloat("Speed", 0f);
         }
     }
 
-    public void AddPoints(int points)
+    private void OnTriggerExit(Collider other)
     {
-        currentScore += points;
-        Debug.Log($"Points added: {points}. Total Score: {currentScore}");
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            if (pressEPrompt) pressEPrompt.SetActive(false);
+            if (dialoguePanel) dialoguePanel.SetActive(false);
+        }
     }
 
-    public void DeductPoints(int points)
+    void AdvanceDialogue()
     {
-        currentScore -= points;
-        Debug.Log($"Points deducted: {points}. Total Score: {currentScore}");
+        if (pressEPrompt) pressEPrompt.SetActive(false);
+        if (dialoguePanel) dialoguePanel.SetActive(true);
+
+        dialogueStep++;
+
+        if (dialogueStep == 1)
+        {
+            dialogueText.text = "There has been a rise of shop thefts recently... I actually caught a teen shoplifting earlier today!";
+        }
+        else if (dialogueStep == 2)
+        {
+            dialogueText.text = "Alright then, stay sharp. Bye!";
+        }
+        else if (dialogueStep == 3)
+        {
+            // End dialogue and walk away
+            if (dialoguePanel) dialoguePanel.SetActive(false);
+            StartCoroutine(WalkAway());
+        }
     }
 
-    public void OnShoplifterCaught()
+    IEnumerator WalkAway()
     {
-        AddPoints(20);
-        Debug.Log("Shoplifter caught successfully!");
-    }
-
-    public void OnThiefEscaped()
-    {
-        DeductPoints(7);
-        Debug.Log("Thief escaped!");
+        if (agent && walkAwayTarget)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(walkAwayTarget.position);
+            if (animator) animator.SetFloat("Speed", 1f);
+        }
+        yield return null;
     }
 }
